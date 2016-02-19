3b3a21c42f87382938aa23385addde875e68c23d
Laurel Farris <laurel@laurelfarris.com>
2016-02-19 10:55:40 -0700

    updates

---
+++ profile.py
@@ -0,0 +1,12 @@
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+#x = np.linspace(100)
+
+gauss = np.random.normal(1000)
+#exp = math.exp(x)
+
+plt.hist(gauss)
+#,plt.plot(x,exp)
+plt.show()
